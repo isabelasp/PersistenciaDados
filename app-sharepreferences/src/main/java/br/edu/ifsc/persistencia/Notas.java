f8c7567d8b3e1d665a8ff3b32c806f43b2c3ed33
ThaisKuminek <thaiseduarda_22@outlook.com>
2019-09-02 22:21:06 -0300

    SheredPreferences SQLLite

--- app-sharepreferences/src/main/java/br/edu/ifsc/persistencia/Notas.java
+++ app-sharepreferences/src/main/java/br/edu/ifsc/persistencia/Notas.java
@@ -2,35 +2,34 @@ package br.edu.ifsc.persistencia;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Notas {
     Context context;
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
-
+    SQLiteDatabase db;
+    public static final String SqliteDataBase = "database";
+    public static final String Nota = "nota";
 
     public Notas(Context c){
         context = c;
-        sharedPreferences = context.getSharedPreferences("notas", Context.MODE_PRIVATE);
-        editor = sharedPreferences.edit();
+        db = context.openOrCreateDatabase(SqliteDataBase,context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE notas (id INTEGER primary key AUTOINCREMENT, nota varchar(255))");
     }
 
     public Boolean salvar(String texto){
-        editor.putString("nota", texto);
-        if (editor.commit()) {
+        return true;
+
+        /*if (editor.commit()) {
             return true;
         }else{
             return false;
-        }
+        }*/
     }
 
     public String recuperar(){
-        if(sharedPreferences.contains("nota")) {
-            return sharedPreferences.getString("nota","");
-        }else {
-            return "Nota não recuperada";
-        }
+            return "";
+
     }
 }
